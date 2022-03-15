0c882659bc8cdf95cf668484671b5aba90787a5d
nitinops <99651852+nitinops@users.noreply.github.com>
2022-03-15 13:36:53 +0000

    Update pyscripting.py

--- pyscripting.py
+++ pyscripting.py
@@ -4,4 +4,4 @@ api_url = "https://jsonplaceholder.typicode.com/todos"
 todo = {"userId": 1, "title": "Buy milk", "completed": False}
 headers = {"Content-Type":"application/json"}
 response = requests.post(api_url, data=json.dumps(todo), headers=headers)
-response.json()
+print{response.json()}
