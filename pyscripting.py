0dc1749ddb4215c2b09a989b4110e214034be86e
nitinops <99651852+nitinops@users.noreply.github.com>
2022-03-16 03:51:54 +0000

    Update pyscripting.py

--- pyscripting.py
+++ pyscripting.py
@@ -1,11 +1,5 @@
 import requests
-import json
-api_url = "https://jsonplaceholder.typicode.com/todos"
-todo = {"userId": 1, "title": "Buy milk", "completed": False}
-headers = {"Content-Type":"application/json"}
-response = requests.put(api_url, json=todo)
+api_url = "https://jsonplaceholder.typicode.com/todos/10"
+todo = {"title": "Mow lawn"}
+response = requests.patch(api_url, json=todo)
 print(response.todo())
-
-
-
-
