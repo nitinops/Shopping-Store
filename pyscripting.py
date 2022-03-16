a406930a8806a7093bf53a93a320258f25f15daf
nitinops <99651852+nitinops@users.noreply.github.com>
2022-03-16 03:56:39 +0000

    Update pyscripting.py

--- pyscripting.py
+++ pyscripting.py
@@ -2,4 +2,5 @@ import requests
 api_url = "https://jsonplaceholder.typicode.com/todos/10"
 todo = {"title": "Mow lawn"}
 response = requests.patch(api_url, json=todo)
+response.json()
 print(response.todo())
