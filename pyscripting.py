3e1b3ea59e472a535319aa83e04dea2ef7d74fa6
nitinops <99651852+nitinops@users.noreply.github.com>
2022-03-18 14:19:20 +0000

    Update pyscripting.py

--- pyscripting.py
+++ pyscripting.py
@@ -1,7 +1,31 @@
 import requests
 import json
-api_url = "https://jsonplaceholder.typicode.com/todos/10"
-todo = {"userId": 1, "title": "Buy milk", "completed": False}
-headers = {"Content-Type":"application/json"}
-response = requests.put(api_url, json=todo)
-print(response.todo())
+
+url = 'https://api.data.gov/ed/collegescorecard/v1/schools.json'
+payload = {
+        'api_key': "api_key_string",
+        '_fields': ','.join([
+            'school.name',
+            'school.school_url',
+            'school.city',
+            'school.state',
+            'school.zip',
+            '2015.student.size',
+        ]),
+        'school.operating': '1',
+        '2015.academics.program_available.assoc_or_bachelors': 'true',
+        '2015.student.size__range': '1..',
+        'school.degrees_awarded.predominant__range': '1..3',
+        'school.degrees_awarded.highest__range': '2..4',
+        'id': '240444',
+    }
+def main():
+    data = requests.get(url, params=payload).json()
+   
+        
+    print(data)
+    
+    
+
+     
+main()
