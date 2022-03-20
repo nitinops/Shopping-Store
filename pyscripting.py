03890437909096a54f5a9b2f8436ecfcb7ab577a
nitinops <99651852+nitinops@users.noreply.github.com>
2022-03-20 12:13:20 +0000

    Update pyscripting.py

--- pyscripting.py
+++ pyscripting.py
@@ -1,5 +1,16 @@
-import requests
-import json
-response_API = requests.get('https://fakestoreapi.com/products')
-data = response_API.text
-print(data)
+pipeline {
+    agent none 
+    stages {
+        stage('Build') { 
+            agent {
+                docker {
+                    image 'python:2-alpine' 
+                }
+            }
+            steps {
+                sh 'python -m py_compile sources/add2vals.py sources/calc.py' 
+                stash(name: 'compiled-results', includes: 'sources/*.py*') 
+            }
+        }
+    }
+}
